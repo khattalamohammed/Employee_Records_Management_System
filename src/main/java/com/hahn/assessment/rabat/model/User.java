50ebf94f6d39f6eff2aa412a9a29fec13eb790c7
MohammedKHATTALA <mkhattala@novelis.io>
2025-01-16 18:26:38 +0100

    configure the db

--- src/main/java/com/hahn/assessment/rabat/model/User.java
+++ src/main/java/com/hahn/assessment/rabat/model/User.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter @Setter
 public class User extends AbstractEntity{
     @Column(name = "username", unique = true, nullable = false)
